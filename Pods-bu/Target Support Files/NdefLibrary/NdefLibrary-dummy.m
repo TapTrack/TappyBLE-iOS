f931cb18159d95ab2853d390061ccac2d0752ffd
dshalaby <dave@taptrack.com>
2021-07-14 16:02:54 -0400

    remove pods-bu

--- Pods-bu/Target Support Files/NdefLibrary/NdefLibrary-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NdefLibrary : NSObject
-@end
-@implementation PodsDummy_NdefLibrary
-@end
--- Pods-bu/Target Support Files/TCMPTappy/TCMPTappy-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TCMPTappy : NSObject
-@end
-@implementation PodsDummy_TCMPTappy
-@end
